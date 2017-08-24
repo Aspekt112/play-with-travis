9ddc9483a3652b763dd99c71993cb2910f6c8bb0
Artem Nurmatov <aspekt112@gmail.com>
2017-08-25 01:39:51 +0300

    This commit should be so failed

--- test.py
+++ test.py
@@ -3,7 +3,22 @@ import unittest
 class NumbersTest(unittest.TestCase):
 
     def test_equal(self):
-        self.assertEqual(1 + 1, 2)
-
+        self.assertEqual(1 + 1, 33sdfs
+                sdfsdfsd
+                sd
+                fd
+                ss
+                df
+                ds
+                fsd
+                fsd
+                fd
+                fd
+                f
+                sdf
+                sd
+                ff
+                s
+                3
 if __name__ == '__main__':
     unittest.main()
