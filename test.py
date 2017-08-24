c57c4d9f5d082318c57b5ff965ed10c894886419
Artem Nurmatov <aspekt112@gmail.com>
2017-08-25 01:44:50 +0300

    Green build again, homework #4 is done

--- test.py
+++ test.py
@@ -3,22 +3,7 @@ import unittest
 class NumbersTest(unittest.TestCase):
 
     def test_equal(self):
-        self.assertEqual(1 + 1, 33sdfs
-                sdfsdfsd
-                sd
-                fd
-                ss
-                df
-                ds
-                fsd
-                fsd
-                fd
-                fd
-                f
-                sdf
-                sd
-                ff
-                s
-                3
+        self.assertEqual(1 + 1, 2)
+
 if __name__ == '__main__':
     unittest.main()
